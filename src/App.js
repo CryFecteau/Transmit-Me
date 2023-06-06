3d989857b2bd72a1b709d46b019deace4820b259
Cry <cry.fecteau@gmail.com>
2023-06-06 14:58:01 -0700

    Styled navBar component with MUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// import { Button } from '@mui/material';
-// import { Add, Settings } from '@mui/icons-material/Delete';
 import { Box, Stack } from '@mui/material';
 import { FeedComponent } from './components/feed-component';
 import { RightBarComponent } from './components/rightBar-component';
 import { SideBarComponent } from './components/sideBar-component';
 import './App.css';
+import { NavBarComponent } from './components/navBar-component';
 
 function App() {
   return (
     <Box>
+      <NavBarComponent />
       <Stack direction="row" spacing={2} justifyContent={"space-between"}>
         <SideBarComponent />
         <FeedComponent />
--- src/components/navBar-component.jsx
+++ src/components/navBar-component.jsx
@@ -1,8 +1,80 @@
-import { Box } from '@mui/material'
+import { Mail, Notifications, SettingsInputAntennaRounded } from '@mui/icons-material';
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, Toolbar, Typography, styled } from '@mui/material'
 import React from 'react'
+import { theme } from '../theme';
 
+const ToolBarStyled = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+});
+
+const SearchBarStyled = styled('div')({
+    backgroundColor: 'white',
+    padding: '0 10px',
+    borderRadius: theme.shape.borderRadius,
+    width: '40%',
+});
+
+const IconsStyled = styled(Box)(({ theme }) => ({
+    display: 'none',
+    alignItems: 'center',
+    gap: '20px',
+    [theme.breakpoints.up('sm')]: {
+        display: 'flex',
+    }
+}));
+
+const UserBoxStyled = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    [theme.breakpoints.up('sm')]: {
+        display: 'none',
+    }
+});
 export const NavBarComponent = () => {
+    const [profileOpen, setProfileOpen] = React.useState(false);
+
     return (
-        <Box>NavBarComponent</Box>
+        <AppBar position='sticky'>
+            <ToolBarStyled>
+                <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block' } }}>Transmit Me</Typography>
+                <SettingsInputAntennaRounded sx={{ display: { xs: 'block', sm: 'none' } }} />
+                <SearchBarStyled>
+                    <InputBase placeholder='Search...' />
+                </SearchBarStyled>
+                <IconsStyled>
+                    <Badge badgeContent={4} color='error'>
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={2} color='error'>
+                        <Notifications />
+                    </Badge>
+                    <Avatar sx={{ width: 30, height: 30 }} alt='Remy Sharp' onClick={e => setProfileOpen(true)} src='https://mui.com/static/images/avatar/1.jpg' />
+                </IconsStyled>
+                <UserBoxStyled>
+                    <Avatar sx={{ width: 30, height: 30 }} alt='Remy Sharp' onClick={e => setProfileOpen(true)} src='https://mui.com/static/images/avatar/1.jpg' />
+                    <Typography variant='h6'>Remy Sharp</Typography>
+                </UserBoxStyled>
+            </ToolBarStyled>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={profileOpen}
+                onClose={e => setProfileOpen(false)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
+        </AppBar>
     )
 }
