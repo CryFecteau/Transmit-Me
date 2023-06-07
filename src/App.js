d49699bb00b49edb0099fcd19435248c324bd0cc
Cry <cry.fecteau@gmail.com>
2023-06-06 19:19:17 -0700

    Styled addPost component with MUI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { RightBarComponent } from './components/rightBar-component';
 import { SideBarComponent } from './components/sideBar-component';
 import './App.css';
 import { NavBarComponent } from './components/navBar-component';
+import { AddPostComponent } from './components/addPost-component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <FeedComponent />
         <RightBarComponent />
       </Stack>
+      <AddPostComponent />
     </Box>
   );
 }
---
+++ src/components/addPost-component.jsx
@@ -0,0 +1,77 @@
+import styled from '@emotion/styled'
+import { AddPhotoAlternateRounded, AddReactionRounded, GroupAddRounded, VideoCallRounded } from '@mui/icons-material'
+import { Avatar, Box, Button, Fab, Modal, Stack, TextField, Tooltip, Typography } from '@mui/material'
+import React from 'react'
+
+const ModalStyled = styled(Modal)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+});
+
+const UserBoxStyled = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    marginTop: '15px',
+});
+
+export const AddPostComponent = () => {
+    const [modelOpen, setModelOpen] = React.useState(false)
+
+    return (
+        <>
+            <Tooltip
+                onClick={() => setModelOpen(true)}
+                title='Add Post'
+                sx={{
+                    position: 'fixed',
+                    bottom: 30,
+                    left: { xs: 'calc(50% - 25px)', md: 30 },
+                }}
+            >
+                <Fab color='primary' aria-label='add post' >
+                    <AddReactionRounded />
+                </Fab>
+            </Tooltip>
+            <ModalStyled
+                open={modelOpen}
+                onClose={() => setModelOpen(false)}
+                aria-labelledby='modal-modal-title'
+                aria-describedby='modal-modal-description'
+            >
+                <Box
+                    width={400}
+                    height={350}
+                    p={3}
+                    borderRadius={5}
+                    bgcolor='white'
+                >
+                    <Typography variant='h6' textAlign={'center'}>Create New Post</Typography>
+                    <UserBoxStyled>
+                        <Avatar sx={{ width: 30, height: 30 }} alt='Remy Sharp' src='https://picsum.photos/50' />
+                        <Typography variant='span'>Remy Sharp</Typography>
+                    </UserBoxStyled>
+                    <TextField
+                        sx={{ width: '100%', mt: 2 }}
+                        id='outlined-multiline-static'
+                        label="What's on your mind?"
+                        multiline
+                        rows={5}
+                        variant='outlined'
+                    />
+                    <Stack direction='row' gap={1} mt={2}>
+                        <AddReactionRounded color='primary' />
+                        <AddPhotoAlternateRounded color='primary' />
+                        <VideoCallRounded color='primary' />
+                        <GroupAddRounded color='primary' />
+                    </Stack>
+                    <Stack direction='row' gap={1} mt={4} justifyContent={'flex-end'}>
+                        <Button variant='outlined' onClick={() => setModelOpen(false)} >Cancel</Button>
+                        <Button variant='contained' onClick={() => setModelOpen(false)}>Post</Button>
+                    </Stack>
+                </Box>
+            </ModalStyled >
+        </>
+    )
+}
